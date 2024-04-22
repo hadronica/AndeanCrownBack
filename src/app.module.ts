017e5b40b5392c0e277dfbf321f545cb19a168e3
Alonso Cortez <practicas.0061@sesitdigital.com>
2024-04-21 19:20:59 -0500

    fixed: mail config ssl

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,6 +27,7 @@ import { FilesModule } from './files/files.module';
       transport:{
         host: process.env.MAIL_HOST,
         port: +process.env.MAIL_PORT,
+        secure:true,
         auth:{
           user:process.env.MAIL_USER,
           pass:process.env.MAIL_PASS,

5e10ef01b02ad6ea3c6b382d5b6bb5198d020530
Alonso Cortez <practicas.0061@sesitdigital.com>
2024-04-24 18:36:14 -0500

    fixed: config port

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(80);
+  await app.listen(8080);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

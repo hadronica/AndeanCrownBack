5deb9aa3d3f2c24ee39508f1650e843507fc71c6
Alonso Cortez <practicas.0061@sesitdigital.com>
2024-05-25 11:50:04 -0500

    fixed: config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,15 @@ import { FilesModule } from './files/files.module';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       //url: process.env.DB_URL,
       host: process.env.DB_HOST,
-      port: +process.env.DB_PORT,
+      // port: +process.env.DB_PORT,
       username: process.env.DB_USER,
       database: process.env.DB_DATABASE,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
-      logging: true,
       ssl:process.env.STAGE==='prod',
       extra:{
         ssl:process.env.STAGE==='prod'?{rejectUnauthorized:false}:null
